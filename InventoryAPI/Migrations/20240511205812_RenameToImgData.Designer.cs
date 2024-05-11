6aaf0f5737a3f50f85fd91486dfe45523c152641
rimvelvet31 <redimportante13@gmail.com>
2024-05-12 05:13:23 +0800

    renamed to ImgData

--- InventoryAPI/Migrations/20240511205812_RenameToImgData.Designer.cs
+++ InventoryAPI/Migrations/20240511205812_RenameToImgData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InventoryAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InventoryAPI.Migrations
 {
     [DbContext(typeof(InventoryContext))]
-    partial class InventoryContextModelSnapshot : ModelSnapshot
+    [Migration("20240511205812_RenameToImgData")]
+    partial class RenameToImgData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");
